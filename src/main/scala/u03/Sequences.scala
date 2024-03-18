11d11af0b3be5b9ab238de4af66b07a0ea44fa39
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2024-03-18 11:27:31 +0100

    [Task 1] Implement min

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -40,7 +40,12 @@ object Sequences: // Essentially, generic linkedlists
       case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper))
       case _          => Nil()
 
-    def min(l: Sequence[Int]): Optional[Int] = ???
+    // def fold[A, B](l: Sequence[A])(acc: B)(mapper: (A, B) => B): B = ???
+
+    def min(l: Sequence[Int]): Optional[Int] = l match
+      case Nil()          => Optional.Empty()
+      case Cons(h, Nil()) => Optional.Just(h)
+      case Cons(h, t)     => min(filter(l)(x => x <= h))
     
 @main def trySequences =
   import Sequences.* 
